1575c93902bdfee18d11cf9c9f16ca68f6e2e604
icrnjavic <32548477+icrnjavic@users.noreply.github.com>
2024-03-01 20:59:49 +0100

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,6 +1,6 @@
 from OWON_XDM1041 import OWONXDM1041
 
-multimeter = OWONXDM1041()
+multimeter = OWONXDM1041() # Port defaults to "/dev/ttyUSB0"
 
 # Get instrument ID
 print("Instrument ID:", multimeter.query_instrument_id())
