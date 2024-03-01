9fac7f242c30e92d1990f6ecfc6b37e08ccab9c6
icrnjavic <32548477+icrnjavic@users.noreply.github.com>
2024-03-01 20:52:42 +0100

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,14 @@
+from OWON_XDM1041 import OWONXDM1041
+
+multimeter = OWONXDM1041()
+
+# Get instrument ID
+print("Instrument ID:", multimeter.query_instrument_id())
+
+# Configure DC voltage
+multimeter.configure_dc_voltage()
+
+
+# Measure value of the set config
+measuredValue = multimeter.measure_value()
+print("Measured value:", measuredValue)
